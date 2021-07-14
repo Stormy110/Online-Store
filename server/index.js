87ea896686682cec206472ef6e4ded95561c51f4
Ian Storms <ianstorms110@yahoo.com>
2021-07-14 11:56:15 -0400

    added client and server

---
+++ server/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const http = require("http");
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const es6Renderer = require("express-es6-template-engine");
+const session = require("session")
+const FileStore = require("session-file-store")(session);
+const app = express();
+const server = http.createServer(app);
+const bcrypt = require("bcryptjs");
+
+
+const logger = morgan("dev");
+const hostname = "0.0.0.0";
+const port = 4000;
+
+
+
+const { layout } = require("./utils")
+const homeController = require("./controllers/home")
+
+
+app.use(logger);
+app.use(helmet);
