4e9c91ffeaff0669294077f271580b809fd7ecfe
Ian Storms <ianstorms110@yahoo.com>
2021-10-29 12:25:16 -0400

    :added nodemon

--- server/index.js
+++ server/index.js
@@ -6,10 +6,11 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const es6Renderer = require("express-es6-template-engine");
 const session = require("session")
-const FileStore = require("session-file-store")(session);
+//const FileStore = require("session-file-store")(session);
 const app = express();
 const server = http.createServer(app);
 const bcrypt = require("bcryptjs");
+
 
 
 const logger = morgan("dev");
@@ -19,8 +20,18 @@ const port = 4000;
 
 
 const { layout } = require("./utils")
-const homeController = require("./controllers/home")
+//const homeController = require("./controllers/home")
 
-
+app.use(express.static('public'));
 app.use(logger);
 app.use(helmet);
+
+
+app.get("/", (req,res)=>{
+    res.send("Hello")
+})
+
+
+server.listen(port,hostname, ()=>{
+    console.log(`Server running at http://${hostname}.${port}`)
+})
